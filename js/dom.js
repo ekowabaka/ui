49ed13f3c017e7ee2497fd163c8e4c2ebad409a6
Ekow Abaka Ainooson <jainooson@gmail.com>
2023-06-28 16:05:18 -0500

    Ready for first deployment

--- js/dom.js
+++ js/dom.js
@@ -1,84 +1,84 @@
-fzui.domUtils = new (function () {
 
-  function getSubsequent(node, direction) {
-    do {
-      node = node[`${direction}Sibling`];
-    } while (node !== null && node.nodeType == Node.TEXT_NODE);
-    return node;
-  }
 
-  /**
-   * Get the dimensions of a given dom node.
-   * 
-   * @param {DomNode} node 
-   * @param {string} dimension 
-   * @param {boolean} margins 
-   * @param {string} margin1 
-   * @param {string} margin2 
-   */
-  function getDimension(node, dimension, margins, margin1, margin2) {
-    let style = window.getComputedStyle(node);
-    if (style['display'] == 'none' || style['display'] == '') {
-      let parent = node.parentNode;
-      let sibling = node.nextSibling;
-      let position = node.style.position;
-      document.body.appendChild(node);
-      node.style.display = 'block';
-      node.style.position = 'absolute';
-      let measured = getDimension(node, dimension, margins, margin1, margin2);
-      node.style.display = 'none';
-      node.style.position = position;
-      if (parent) {
-        parent.insertBefore(node, sibling);
-      }
-      return measured;
+function getSubsequent(node, direction) {
+  do {
+    node = node[`${direction}Sibling`];
+  } while (node !== null && node.nodeType == Node.TEXT_NODE);
+  return node;
+}
+
+/**
+  * Get the dimensions of a given dom node.
+  * 
+  * @param {DomNode} node 
+  * @param {string} dimension 
+  * @param {boolean} margins 
+  * @param {string} margin1 
+  * @param {string} margin2 
+  */
+function getDimension(node, dimension, margins, margin1, margin2) {
+  let style = window.getComputedStyle(node);
+  if (style['display'] == 'none' || style['display'] == '') {
+    let parent = node.parentNode;
+    let sibling = node.nextSibling;
+    let position = node.style.position;
+    document.body.appendChild(node);
+    node.style.display = 'block';
+    node.style.position = 'absolute';
+    let measured = getDimension(node, dimension, margins, margin1, margin2);
+    node.style.display = 'none';
+    node.style.position = position;
+    if (parent) {
+      parent.insertBefore(node, sibling);
     }
-    return parseInt(style[dimension])
-      + parseInt(style[`padding-${margin1}`]) + parseInt(style[`padding-${margin2}`])
-      + (margins ? parseInt(style[`margin-${margin1}`]) + parseInt(style[`margin-${margin2}`]) : 0);
+    return measured;
   }
+  return parseInt(style[dimension])
+    + parseInt(style[`padding-${margin1}`]) + parseInt(style[`padding-${margin2}`])
+    + (margins ? parseInt(style[`margin-${margin1}`]) + parseInt(style[`margin-${margin2}`]) : 0);
+}
 
-  this.nextSibling = function (node) {
+export const DomUtilities = {
+  nextSibling: function (node) {
     return getSubsequent(node, 'next')
-  }
+  },
 
-  this.previousSibling = function (node) {
+  previousSibling: function (node) {
     return getSubsequent(node, 'previous')
-  }
+  },
 
-  this.toggleClass = function (node, className) {
+  toggleClass: function (node, className) {
     if (node.classList.contains(className)) {
       node.classList.remove(className)
     } else {
       node.classList.add(className)
     }
-  }
+  },
 
-  this.toggleStyleProperty = function(node, styleName, value1, value2) {
+  toggleStyleProperty: function (node, styleName, value1, value2) {
     if (node.style[styleName] === value1) {
       node.style[styleName] = value2
     } else {
       node.style[styleName] = value1
     }
-  }
+  },
 
-  this.outerHeight = function (node, margins) {
+  outerHeight: function (node, margins) {
     return getDimension(node, 'height', margins, 'top', 'bottom')
-  }
+  },
 
-  this.outerWidth = function (node, margins) {
+  outerWidth: function (node, margins) {
     return getDimension(node, 'width', margins, 'left', 'right')
-  }
+  },
 
-  this.siblings = function(node) {
+  siblings: function (node) {
     let siblings = [];
-    for(let sibling = node.parentNode.firstChild ;sibling; sibling = sibling.nextSibling) {
-      if(sibling === node || sibling.nodeType === Node.TEXT_NODE) {
+    for (let sibling = node.parentNode.firstChild; sibling; sibling = sibling.nextSibling) {
+      if (sibling === node || sibling.nodeType === Node.TEXT_NODE) {
         continue;
       }
       siblings.push(sibling);
     }
     return siblings;
   }
-})();
-
+}
--- js/nav.js
+++ js/nav.js
@@ -1,27 +1,62 @@
+import {DomUtilities} from './dom.js'
+
+/**
+ * Holds all navs that are currently active.
+ */
+const allNavs = [];
+
 /**
  * Implements the highlighting of the current tab or pill for tab and pill
  * user interfaces.
  */
 
 export function init() {
-    document.querySelectorAll(".tabs").forEach(x => new Nav(x));
+    document.querySelectorAll("ul.tabs").forEach(x => new Nav(x));
 }
 
+/**
+ * Represents a navigational unit.
+ */
 class Nav {
+    /**
+     * The container holding the list of tabs.
+     */
     #tabsContainer;
     #tabs;
     #panesContainer;
     #panes
+    #activeTab;
 
     constructor (container) {
+        this.#activeTab = 0;
         this.#tabs = container.querySelectorAll('li');
-        this.#tabs.forEach(tab => tab.addEventListener('click', e => this.#tabClicked(e)));
+        this.#tabs.forEach((tab, i) => tab.addEventListener('click', e => {
+            this.showTab(i);
+            if (e.target.classList.contains("active")) {
+                this.#activeTab = i;
+            }
+        }));
         this.#tabsContainer = container;
+        this.#panesContainer = DomUtilities.nextSibling(container);
+
+        if (this.#panesContainer !== null) {
+            this.#panes = this.#panesContainer.querySelectorAll("div");
+        } else {
+            this.#panes = [];
+        }
+
+        this.showTab(this.#activeTab);
     }
 
-    #tabClicked (event) {
+    showTab (index) {
         this.#tabs.forEach(x => x.classList.remove("active"));
-        event.target.classList.add("active");
+        this.#panes.forEach(x => x.classList.remove("active"));
+        this.#tabs[index].classList.add("active");   
+        this.#activeTab = index;
+
+        if (index < this.#panes.length) {
+            this.#panes[index].classList.add("active");
+        }
     }
 }
 
